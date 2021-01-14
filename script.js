6206d71e9e208a1c639d3edae085e46d7b38145a
Sean-Drew <SDrew989@gmail.com>
2021-01-14 09:52:57 -0500

    Add new functions
    Added functions to handle clearing default values, adding tasks and
    removing tasks.

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-function get_todos() {
+function getTodos() {
     let todos = new Array
     let todos_str = localStorage.getItem('todo')
     if (todos_str !== null) {
         todos = JSON.parse(todos_str)
     }
     return todos
+}
+
+function addTask() {
+    let task = document.getElementById('task').value
+    let todos = getTodos()
+    todos.push(task)
+    localStorage.setItem('todo', JSON.stringify(todos))
+    show()
+    return false
+}
+
+function clearDefault(a) {
+    if (a.defaultValue == a.value) {
+        a.value = ''
+    }
+}
+
+function removeTask() {
+    let id = this.getAttribute('id')
+    let todos = getTodos()
+    todos.splice(id, 1)
+    localStorage.setItem('todo', JSON.stringify(todos))
+    show()
+    return false
 }
